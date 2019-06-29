65fd114b628b3badadc8472d114b5da16a7e53af
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-29 21:12:33 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_active_to_students.rb
+++ db/migrate/002_add_active_to_students.rb
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 class AddActiveToStudents < ActiveRecord::Migration
     def change
       add_column :students, :active, :boolean, default: false, null: false
     end
   end
   
+=======
+class AddActiveToStudents < ActiveRecord::Migration
+  def change
+    add_column :students, :active, :boolean, default: false, null: false
+  end
+end
+>>>>>>> 6a4742ae11058245aeadd2fd9a235e399a445c7c
